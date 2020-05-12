028f396ad2981f80ccb346099e293e4eb20a8c79
jrestrepo922 <tinto922@gmail.com>
2020-05-12 11:25:33 -0400

    patch /cities/:id route was created

--- app/controllers/cities_controller.rb
+++ app/controllers/cities_controller.rb
@@ -86,12 +86,71 @@ class CitiesController < ApplicationController
     end
 
     patch "/cities/:id" do 
-        binding.pry 
+        if logged_in? 
+            @country = current_user.countries.find_by(id: params[:id]) 
+            if @country
+                # country is change 
+                if @country.name != params[:country][:name_new]
+                    # will need to check if is submitted empty
+                    if params[:country][:name_new].empty?
+                        redirect "/cities/#{@country.id}/edit"
+                    end 
+                    # will need to delete the cities that belong to the country
+                    @country.cities.delete_all
+                    # update the country 
+                    @country.update(name: params[:country][:name_new])
+                    # check to see if information about the cities were enter and if is create new ones
+                    params[:country][:cities].each do |city|
+                        if !city[:name].empty? && !city[:trip_details].empty?
+                            new_city = current_user.cities.create(name: city[:name], trip_details: city[:trip_details])
+                            @country.cities << new_city 
+                        end 
+                    end 
+                    redirect "/cities/#{@country.id}"
+                # country is the same 
+                elsif @country.name == params[:country][:name_new]
+
+                    @country.cities.each_with_index do |city, index|
+                        # city and trip details is the same 
+                        if city.name == params[:country][:cities][index][:name] && city.trip_details == params[:country][:cities][index][:trip_details]
+                            redirect "/cities/#{@country.id}"
+                        elsif city.name != params[:country][:cities][index][:name] && city.trip_details == params[:country][:cities][index][:trip_details]
+                            city.name = params[:country][:cities][index][:name]
+                            city.save
+                            redirect "/cities/#{@country.id}"
+                        elsif city.name == params[:country][:cities][index][:name] && city.trip_details != params[:country][:cities][index][:trip_details]
+                            city.trip_details = params[:country][:cities][index][:trip_details]
+                            city.save
+                            redirect "/cities/#{@country.id}"
+                        else
+                            city.name = params[:country][:cities][index][:name]
+                            city.trip_details = params[:country][:cities][index][:trip_details]
+                            city.save
+                            redirect "/cities/#{@country.id}"
+                        end 
+                    end 
+        
+                end 
+            else 
+                redirect "/cities"
+            end 
+        else 
+            redirect "/login"
+        end 
     end 
 
     #DELETE
     delete "/cities/:id" do 
-
+        if logged_in?
+            @country = current_user.countries.find_by(id: params[:id])
+            if @country
+                @country.cities.delete_all
+                @country.delete
+            end 
+            redirect '/cities'
+        else 
+            redirect 'login'
+        end
     end 
 
 end 
