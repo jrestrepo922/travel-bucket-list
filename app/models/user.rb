7acc00020db47eb0abcbeeaff0e44f573a7c4911
jrestrepo922 <tinto922@gmail.com>
2020-05-12 13:30:22 -0400

    added validation to sign up page

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
     has_many :cities
     has_many :countries, :through => :cities
     has_secure_password
+
+    #adding validators
+    validates :username, :email, :password, presence: true
+    validates :username, :email, uniqueness: true
 end 
